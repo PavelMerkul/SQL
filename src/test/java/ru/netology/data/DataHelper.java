44769a99edcb7edec61b05d9c3cd7a6021a832bd
Pavel MSK <sonev@bk.ru>
2024-11-24 23:23:21 +0300

    fix

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -15,7 +15,6 @@ public class DataHelper {
         return new AuthInfo("vasya", "qwerty123");
     }
 
-
     private static String generateRandomLogin() {
         return faker.name().username();
     }
@@ -24,48 +23,22 @@ public class DataHelper {
         return faker.internet().password();
     }
 
-    public static AuthInfo generateRandomUser() {
+    public static AuthInfo generateRandomUser () {
         return new AuthInfo(generateRandomLogin(), generateRandomPassword());
     }
 
     public static VerificationCode generateRandomVerificationCode() {
-        var verificationCode = new VerificationCode(faker.numerify("######"));
-        return verificationCode;
+        return new VerificationCode(faker.numerify("######"));
     }
 
     @Value
     public static class AuthInfo {
         String login;
         String password;
-
-        public AuthInfo(String s, String s1) {
-            
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public void setLogin(String login) {
-            this.login = login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
     }
 
     @Value
     public static class VerificationCode {
         String code;
-
-        public VerificationCode(String numerify) {
-            
-        }
-
-        public String getCode() {
-            return null;
-        }
     }
-
 }
